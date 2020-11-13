1b66692b9864c8565dfe07ab08558369573056fe
Bruno Martins <bgcngm@gmail.com>
2020-11-13 10:56:44 +0000

    sdm632-common: Explicitly build additional IMS stack dependency
    Commit 160fc65 removed the lib from PRODUCT_PACKAGES with the
    assumption that it was implicitely built while having
    TARGET_USES_QTI_CAMERA flag set. Turns out that build system
    skips compilation of vendor.qti.hardware.camera.device@1.0
    that is supposed to land in /system_ext/lib64 and this one is
    a required dependency of the current IMS stack.
    
    Change-Id: I7fdbdc1b837df670ecaf6775946784017c68134c

--- common.mk
+++ common.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0:64 \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl:32 \
