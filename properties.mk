c162265f2234493b5d91553718eed496fa5cb5a2
Jarl-Penguin <jarlpenguin@outlook.com>
2021-10-29 00:59:00 +0300

    sdm632-common: Update display props from stock
    * Kang all but debug.egl/sf.hw and debug.sf.latch_unsignaled
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I39e7679fca19ae26b23379e143bf18619b2e5099

--- properties.mk
+++ properties.mk
@@ -70,17 +70,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Color Mode
 PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.hwc_set_default_colormode=true \
+    debug.egl.hw=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    debug.sf.disable_backpressure=1 \
     dev.pm.dyn_samplingrate=1 \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    ro.vendor.display.cabl=0
 
 # Factory reset partition
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -188,17 +189,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.use_color_management=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
 
 # Time daemon
 PRODUCT_PROPERTY_OVERRIDES += \
