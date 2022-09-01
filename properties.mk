ee7a59e646156e1cb0a6bec64b998b9ee4e7103a
jro1979oliver <jroliveira.oliveira301@gmail.com>
2022-09-01 11:48:34 +0000

    sdm632-common: Add tethering usb props
    Change-Id: I841feaa53db18b3e765a0a864ca062b6ff4ee27e

--- properties.mk
+++ properties.mk
@@ -234,6 +234,12 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.usb.rmnet.func.name=rmnet_bam
+    vendor.usb.rmnet.inst.name=rmnet
+    vendor.usb.rndis.func.name=rndis_bam
+
 # Voice assistant
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
