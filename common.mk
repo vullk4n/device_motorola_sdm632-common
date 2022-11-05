61dc7afd1dd3c8fae9c045436e55a35c328bbd73
Nolen Johnson <johnsonnolen@gmail.com>
2022-11-05 01:47:16 -0400

    sdm632-common: Migrate to *.prop
    Change-Id: Ide3493a0d98706e46e44388e232a811b1d10fbd4

--- common.mk
+++ common.mk
@@ -27,9 +27,6 @@ $(call inherit-product, vendor/motorola/sdm632-common/sdm632-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Properties
--include $(LOCAL_PATH)/properties.mk
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
