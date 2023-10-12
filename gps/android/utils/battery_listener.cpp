a99ddfb326ee175b89caf904b34386f9b0555df4
ralph950412 <ralph950412@gmail.com>
2023-10-11 21:43:37 -0300

    sdm632-common: gps: Fix unqualified-std-cast-call warning error: unqualified call to std::move [-Werror,-Wunqualified-std-cast-call]             mIpcRecver(move(ipcRecver)) {}                        ^                        std::

--- gps/android/utils/battery_listener.cpp
+++ gps/android/utils/battery_listener.cpp
@@ -182,13 +182,14 @@ BatteryListenerImpl::~BatteryListenerImpl()
 {
     {
         std::lock_guard<std::mutex> _l(mLock);
-        if (mHealth != NULL)
+        if (mHealth != NULL) {
             mHealth->unlinkToDeath(this);
             auto r = mHealth->unlinkToDeath(this);
             if (!r.isOk() || r == false) {
                 LOC_LOGe("Transaction error in unregister to HealthHAL death: %s",
                         r.description().c_str());
             }
+        }
     }
     mDone = true;
     mThread->join();
