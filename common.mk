b57ec84bc7a25eda80a41de322f975e68a84a68a
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-10-22 17:10:04 -0300

    sdm632-common: Build android.frameworks.sensorservice Needed to get cameras working on Android 14.
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- common.mk
+++ common.mk
@@ -121,7 +121,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     camera.device@3.2-impl:32 \
     libbson.vendor \
-    libxml2
+    libxml2 \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
